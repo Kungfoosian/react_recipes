11fe0bae9d9ae0785ab4e65c1e71719f2e59610a
Anthony <nemothetuna@gmail.com>
2022-05-28 19:11:18 -0700

    can now toggle between by name and ingredient tab

--- src/2.0.Search.js
+++ src/2.0.Search.js
@@ -2,7 +2,20 @@ import Form from './3.0.Form';
 
 function Search(props) {
     const toggleTab = tabId => {
-        console.log(tabId);
+        let otherTabId = tabId === 'form-by-name' ? 'form-by-ingredient' : 'form-by-name';
+
+        const otherForm = document.getElementById(otherTabId);
+
+        otherForm.className = '';
+
+        otherForm.classList.add('hidden');
+        
+        const form = document.getElementById(tabId);
+
+        form.classList.remove('hidden');
+        
+        if (tabId === 'form-by-name') form.classList.add('by-name');
+        else form.classList.add('by-ingredient');
     }
 
     return (
@@ -13,7 +26,6 @@ function Search(props) {
             </div>
 
             <div>
-                {/* <Form className='by-name' id='form-by-name' byIngredient={false}  /> */}
                 <Form className='hidden' id='form-by-name' byIngredient={false}  />
                 <Form className='by-ingredient' id='form-by-ingredient' byIngredient={true} />
             </div>
