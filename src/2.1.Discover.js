855f2d3f76957f7b150b01e4c0023a0205c15a48
Anthony <nemothetuna@gmail.com>
2022-06-03 22:27:47 -0700

    styled discover ,add keyword component

--- src/2.1.Discover.js
+++ src/2.1.Discover.js
@@ -1,10 +1,12 @@
-import Card from './3.1.Card'
+import Card from './3.1.Card';
+import Keywords from './3.2.Keywords';
 
 
 function Discover(props) {
     return (
         <div className={props.className} id={props.id} >
             <Card className='card' />
+            <Keywords className='keywords' keywords={['keyword 1', 'keyword 2', 'keyword 3', 'keyword 4', 'keyword 1', 'keyword 2', 'keyword 3', 'keyword 4']} />
         </div>);
 }
 
