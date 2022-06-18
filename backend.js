886c7024be26b03225dec1cd27cce791401fdd47
Anthony <nemothetuna@gmail.com>
2022-06-17 20:25:10 -0700

    Working on api call for recipes by name

--- backend.js
+++ backend.js
@@ -1,14 +1,37 @@
 require('dotenv').config();
 
+const axios = require('axios');
+const cors = require('cors');
 const express = require('express');
 const app = express();
 const PORT = 8000;
 
+app.use(cors());
+
 app.listen(PORT);
 console.log(`Server listening on ${PORT}`)
 
-console.log(process.env.TASTYAPI_KEY);
-
 app.get('/', (req, res) => {
   res.json('Hi');
+})
+
+app.get('/recipes-by-name', (req, res) => {
+  let recipe = req.query.q;
+  // console.log(req.query.q);
+  const options = {
+    method: 'GET',
+    url: 'https://tasty.p.rapidapi.com/recipes/list',
+    params: {from: '0', size: '5', q:recipe},
+    headers: {
+      'X-RapidAPI-Key': process.env.TASTYAPI_KEY,
+      'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+    }
+  };
+  
+  axios.request(options)
+  .then(response => {
+    res.json(response.data);
+  }).catch(function (error) {
+    console.error(error);
+  });
 })
--- src/4.0.FormByName.js
+++ src/4.0.FormByName.js
@@ -1,11 +1,42 @@
-import Input from './5.1.Input';
-import ActionBtn from './5.0.ActionBtn';
+import { useState } from "react";
 
-function FormByName(props){
+const SERVER_URL = 'http://localhost:8000'
+const axios = require('axios');
+
+export default function FormByName(props){
+    const [recipe, editRecipe] = useState('');
+
+    const updateRecipe = event => {
+        editRecipe(event.target.value);
+        // console.log(recipe);
+    }
+
+    const searchRecipe = () => {
+        console.log(`searching for ${recipe}`);
+
+        const options = {
+            method: 'GET',
+            url: `${SERVER_URL}/recipes-by-name`,
+            params: {
+                q: recipe,
+            }
+          };
+          
+          axios.request(options)
+          .then(response => {
+            // res.json(response.data);
+            console.log(response.data);
+          }).catch(function (error) {
+            console.error(error);
+          });
+    }
+
     return (<div id={props.id} className={props.className}>
-        <Input placeholder='e.g. Beef Stroganoff' />
-        <ActionBtn type='search' />
+        {/* <Input placeholder='e.g. Beef Stroganoff' /> */}
+        <label htmlFor='name-input'>
+            <input type="text" name='name-input' placeholder='e.g. Carrot Cake' onChange={updateRecipe} />
+        </label>
+
+        <button onClick={searchRecipe}>SEARCH</button>
     </div>)
 }
-
-export default FormByName;
